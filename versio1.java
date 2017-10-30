d934cbe130a12c54388ae77d28ee27ba670ee236
Magga Oula P <magga@vdi-cubbli-029.ad.helsinki.fi>
2017-10-30 02:36:47 +0200

    lisätty kolmikoiden kirjaaminen

--- versio1.java
+++ versio1.java
@@ -1,24 +1,34 @@
+import java.util.*;
+import java.io.*;
 class Versio1 {
 	// tässä lasketaan summia s=a^2+b^2 ja selvitetään
 	// onko luku sqrt(s) kokonaisluku
-	public static void main(String[] arks) {
+	public static void main(String[] arks) throws Exception {
 		long t1 = System.currentTimeMillis();
 		// ajetaan m minuuttia
 		int m = 10;
 		// muuttujien alustus
 		int a = 1;
 		int kolmikoita = 0;
+		List<int[]> kolmikot = new ArrayList((int)3e6);
 		while (System.currentTimeMillis()-t1 < m*60*1e3) {
 			for (int b = 1; b <= a; b++) {
 				int s = a*a+b*b;
-				int u = (int)Math.sqrt(s);
-				if (u*u == s) {
+				int c = (int)Math.sqrt(s);
+				if (c*c == s) {
 					kolmikoita++;
+					kolmikot.add(new int[]{a,b,c});
 				}
 			}
 			a++;
 		}
 		System.out.println("Kolmikoita löytyi ajalla " + (System.currentTimeMillis()-t1)
 					+ " ms yhteensä " + kolmikoita);
+		FileWriter tiedosto = new FileWriter("kolmikot.data");
+		for (int[] kolmikko : kolmikot) {
+			tiedosto.write("("+kolmikko[0]+","+kolmikko[1]+","+kolmikko[2]+")\n");
+		}
+		tiedosto.close();
+		System.out.println("Kolmikot kirjoitettiin tiedostoon kolmikot.data");
 	}
 }
